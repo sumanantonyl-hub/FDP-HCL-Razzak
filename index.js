536b29e362b58eb7eec89441de2bb78a13ca2496
sumanantonyl-hub <sumanantony.l@jainuniversity.ac.in>
2026-06-25 14:33:53 +0530

    v3

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const parser = require('body-parser')
 const db = require('./connection')
 const dbApi = require('./CorporateDbController')
 const uApi = require('./UserController')
+require('dotenv').config()
 
 db()
 app.use(parser.json())
 app.use('/api',dbApi)
 app.use('/auth',uApi)
 
-const PORT=2222
+const PORT=process.env.PORT
 app.listen(PORT,()=>{
     console.log(`API Running @ ${PORT}`)
 })
